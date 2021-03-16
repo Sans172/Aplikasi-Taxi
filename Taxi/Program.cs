127bc54c6adb73b255669fee3c1e31ac2d292409
Sans172 <sandywildani170@gmail.com>
2021-03-16 14:51:18 +0700

    Add project files.

---
+++ Taxi/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Taxi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //membuat taxi
+            Taxi taxi = new Taxi();
+
+            //pengesetan nilai properti
+            taxi.drivername = "Azriel";
+            taxi.onduty = true;
+            taxi.numpassenger = 10;
+
+            taxi.Taxiinfo();
+            taxi.pickuppassenger();
+            taxi.dropoffpassenger();
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Taxi/Taxi.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+
+namespace Taxi
+{
+    class Taxi
+    {
+        public string drivername { get; set; }
+        public bool onduty { get; set; }
+        public int numpassenger { get; set; }
+
+        public void Taxiinfo()
+        {
+            Console.WriteLine("Nama Driver :{0}", drivername);
+            if (onduty == true)
+            {
+                Console.WriteLine("Taxi On Duty: Yes");
+            }
+            if (onduty == false)
+            {
+                Console.WriteLine("Taxi On Duty:NO");
+            }
+            Console.WriteLine("Jumlah Penumpang: {0}",numpassenger);
+        }
+
+        public void pickuppassenger()
+        {
+            Console.WriteLine("{0} Sedang Menjemput Penumpang", drivername);
+        }
+        public void dropoffpassenger()
+        {
+            Console.WriteLine("{0} Selesai Mengantarkan Penumpang", drivername);
+        }
+    }
+}
